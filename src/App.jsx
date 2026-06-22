87390a1b9ad176626d55e87860932d7db4a6a4da
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2026-06-22 09:51:14 -0300

    commit tasks props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import AddTasks from "./components/AddTasks";
 import Tasks from "./components/Tasks";
 import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programaçao para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 1,
+      title: "Estudar matemática ",
+      description:
+        "Estudar matemática para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <>
-      <h1 className="text-red-500">Gerenciador Tarefas</h1>
-      <AddTasks />
-      <Tasks />
-    </>
+    <div className="w-screen h-screen bs-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador Tarefas
+        </h1>
+        <AddTasks />
+        <Tasks tasks={tasks} />
+      </div>
+    </div>
   );
 }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,6 @@
-const Tasks = () => {
-  return (
-    <div>
-      <h1>Tasks</h1>
-    </div>
-  );
+const Tasks = (props) => {
+  console.log(props);
+  return <h1>{props.tasks[0].title}</h1>;
 };
 
 export default Tasks;

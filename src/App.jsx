07fe338621178473b56714a57e4587d49b4e9fa8
rodrigo-gr-pereira <rodrigogr.pereira@gmail.com>
2026-06-22 10:09:15 -0300

    commit tasks + lucide icon

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   ]);
 
   return (
-    <div className="w-screen h-screen bs-slate-500 flex justify-center p-6">
+    <div className="w-screen h-screen bg-cyan-900 bs-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador Tarefas
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,16 @@
 const Tasks = (props) => {
-  console.log(props);
-  return <h1>{props.tasks[0].title}</h1>;
+  return (
+    <ul className="space-y-4 p-6 bg-slate-500 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex">
+          <button className="bg-slate-400 w-full text-white p-2 rounded-md ">
+            {task.title}
+          </button>
+          <button></button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Tasks;
